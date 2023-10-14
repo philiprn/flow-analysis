70b18f4ed5c1b6efa9b2ac669c3869922517824a
Philip Rutten <philiprutten1@gmail.com>
2023-10-14 13:48:07 +0200

    Update.

--- gmm-EM-fitter.R
+++ gmm-EM-fitter.R
@@ -1,6 +1,6 @@
 library(Rcpp)
 library(RcppArmadillo)
-library(flowCore)
+library(data.table)
 
 ################################################################################
 # EM algorithm on MULTIPLE samples
@@ -11,43 +11,50 @@ K <- commandArgs(trailingOnly=TRUE)
 K <- strtoi(K)
 
 ################################################################################
-# DATA
-
-directory <- "/net/beegfs/cfg/projects/mrd_mfc/scratch-share/AML_NBM/patients/"
-patients <- dir(directory)
-M <- length(patients)
-
-################################################################################
 # SOURCE function
 
-sourceCpp("logstepper_logL.cpp")
+sourceCpp("ompstepper.cpp")
+
+################################################################################
+# DATA
+
+patients <- read.csv("P5samples100_01.csv")[,1]
+setwd("/net/beegfs/scratch/prutten/H132_P5denovo")
+#patients <- dir()
+#patients <- patients[c(1:2,sample(c(3:195,200),80,replace=FALSE),196:199,201:214)]
+M <- length(patients)
 
 ################################################################################
 # Create subsample
 
-subsample <- matrix(NA, M * 1e4, 8)
+Msub <- 50
+#set.seed(1)
+patientsubsample <- patients[sample(M,Msub,replace=FALSE)]
+subsample <- matrix(NA, Msub * 1e4, 8)
 
-for (s in 1:M) {
-  setwd(paste(directory,patients[s],sep=''))
-  fcsFile <- dir()[2]
-  subset <- read.FCS(fcsFile, transformation="linearize")
-  c1<-which(colnames(subset@exprs)=="FITC-A")
-  Y1 <- subset@exprs[,c1:(c1+7)]
+cat("Model",K,"start building subsample from",Msub,"out of",M,"samples.","\n")
+
+for (s in 1:Msub) {
+  fcsFile <- patientsubsample[s]
+  Y1 <- fread(file=fcsFile)
   Y1 <- as.matrix(Y1)
   
-  a <- (s - 1) * 10000 + 1
-  b <- a + 10000 - 1
-  subsample[a:b,] <- Y1[sample(nrow(Y1), 10000, replace=FALSE),]
+  a <- (s - 1) * 1e4 + 1
+  b <- a + 1e4 - 1
+  if (nrow(Y1) >= 1e4){
+    subsample[a:b,] <- Y1[sample(nrow(Y1), 1e4, replace=FALSE),]
+  } else {
+    subsample[a:b,] <- Y1[sample(nrow(Y1), 1e4, replace=TRUE),]
+  }
 }
-
 
 ################################################################################
 # Initialize parameters
 
 p<-8
-convergence_threshold <- 5e-3
+convergence_threshold <- 1e-3
 
-means <- kmeans(subsample, K, iter.max=1000, nstart=10, algorithm="MacQueen")$centers
+means <- kmeans(subsample, K, iter.max=1000, algorithm="MacQueen")$centers
 Sigmas <- t(matrix(rep(diag(p), K), p, K*p))
 weights <- matrix(1/K, length(patients), K)
 
@@ -58,6 +65,8 @@ iter <- 1
 # EM loop
 
 while (convergence > convergence_threshold) {
+  cat("Model",K,"iteration",iter,format(Sys.time(),usetz=TRUE),"\n")
+
   # Accumulators
   A0 <- matrix(0, length(patients), K) # rep(0,K)
   A1 <- matrix(0, K, p)
@@ -66,13 +75,13 @@ while (convergence > convergence_threshold) {
   logL <- 0
   
   for (s in 1:M) {
-    setwd(paste(directory,patients[s],sep=''))
-    fcsFile <- dir()[2]
-    subset <- read.FCS(fcsFile, transformation="linearize")
-    c1<-which(colnames(subset@exprs)=="FITC-A")
-    Y1 <- subset@exprs[,c1:(c1+7)]
+    fcsFile <- patients[s]
+    Y1 <- fread(file=fcsFile)
     Y1 <- as.matrix(Y1)
-    
+    #if (nrow(Y1) > 5e5) {
+    #  set.seed(1)
+    #  Y1 <- Y1[sample(1:nrow(Y1),5e5,replace=FALSE),]
+    #}
     A0_ <- A0[s,]
     
     logL <- logL + logstep(Y1, weights[s,], means, Sigmas, A0_, A1, A2)
@@ -97,13 +106,12 @@ while (convergence > convergence_threshold) {
 }
 
 ################################################################################
-
 # STORE RESULTS
-setwd("~/results")
+setwd("/net/beegfs/scratch/prutten/results/run12")
 
-write.csv(weights, paste("weights-MRD_NBM-48-samples-",toString(K),".csv",sep=''))
-write.csv(means, paste("means-MRD_NBM-48-samples-",toString(K),".csv",sep=''))
-write.csv(Sigmas, paste("covs-MRD_NBM-48-samples-",toString(K),".csv",sep=''))
+write.csv(weights, paste("weights-H132-P5-100-samples-",toString(K),".csv",sep=''),row.names=FALSE)
+write.csv(means, paste("means-H132-P5-100-samples-",toString(K),".csv",sep=''),row.names=FALSE)
+write.csv(Sigmas, paste("covs-H132-P5-100-samples-",toString(K),".csv",sep=''),row.names=FALSE)
 
 
 
